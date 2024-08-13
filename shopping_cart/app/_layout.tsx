e4c58c4ed533e5b9aaf32125f48a195a3e0af867
ValkarVarg <mikewilliamstfb@gmail.com>
2024-08-13 15:53:13 +0100

    adds screens

--- shopping_cart/app/_layout.tsx
+++ shopping_cart/app/_layout.tsx
@@ -1,37 +1,24 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { Stack, Link } from "expo-router";
+import { Pressable, Text } from "react-native";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerRight: () => (
+            <Link href="/cart" asChild>
+            <Pressable>
+              <Text>Checkout</Text>
+            </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="cart"
+      />
+    </Stack>
   );
 }
