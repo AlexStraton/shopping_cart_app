f0ecdde665daf6a92cc32ad937cbd3ccebd4e675
Alex Straton <alexandrastraton1@gmail.com>
2024-08-14 10:04:57 +0100

    added more products in the data file

--- back-end/db/data/products.ts
+++ back-end/db/data/products.ts
@@ -1,10 +1,51 @@
-const products = [
+const products =  [
   {
     product_name: "soap",
     price: 99,
-    product_image_url:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61eNgxUDg-3wgANvffiHQZHTVq3L1l80trQ&s",
+    product_image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61eNgxUDg-3wgANvffiHQZHTVq3L1l80trQ&s",
     description: "Used to wash oneself",
+  },
+  {
+    product_name: "shampoo",
+    price: 150,
+    product_image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnyrQsZBvyNUbxEaezl3XUf0YQbSx5xzehfw&s",
+    description: "Cleans and conditions hair",
+  },
+  {
+    product_name: "toothpaste",
+    price: 85,
+    product_image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3FfqD4dp9klB2wSEKuasxEyLGRn4c6HthAq&s",
+    description: "Helps in cleaning teeth",
+  },
+  {
+    product_name: "towel",
+    price: 299,
+    product_image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ4M0_sKYNUdO4kM-QsCqGiDjL5BQc0KxnvQ&s",
+    description: "Used for drying the body",
+  },
+  {
+    product_name: "deodorant",
+    price: 199,
+    product_image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHqEQw_vDQtbZ0fFZJUnNGZ5HxGc7Zqz3kqw&s",
+    description: "Reduces body odor",
+  },
+  {
+    product_name: "lotion",
+    price: 175,
+    product_image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGkD0Ih77-hzr2PDhx_qopMkKsKzg5NkDxMA&s",
+    description: "Moisturizes the skin",
+  },
+  {
+    product_name: "comb",
+    price: 45,
+    product_image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1PE0SlxHHTZfeTufyqB2pG9ctFoZvHNVrxQ&s",
+    description: "Helps to style hair",
+  },
+  {
+    product_name: "facial cleanser",
+    price: 120,
+    product_image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp_HgNjIx8s1XYPg6fBjKg8IY_nzTaU5AaVQ&s",
+    description: "Cleans the face skin deeply",
   },
 ];
 
